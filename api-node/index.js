0bd0366bc0162125bef3b2ae213982b6a9bd79c6
rogeriobnr <rogeriobnr@gmail.com>
2023-04-22 18:41:04 -0300

    feat: create a route to method get

--- api-node/index.js
+++ api-node/index.js
@@ -2,11 +2,28 @@ const express = require('express');
 const app = express();
 
 
-app.get('/users', (req, res) =>{
-    return res.json({msg: 'User'});
+app.get('/users', (req, res) => {
+
+    //user array
+    const users =
+        [
+            {
+                id: 1,
+                name: 'John Doe'
+            },
+            {
+                id: 2,
+                name: 'Jane Doe'
+            }
+
+        ]
+
+    return res.json(user);
 
 });
 
-app.listen(3000, () =>{
-    console.log('Server is running on port 3000');
+
+
+app.listen(3000, () => {
+    console.log('\n Server is running on port 3000');
 });
