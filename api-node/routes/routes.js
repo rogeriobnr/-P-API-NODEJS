a5a9f21a7f82cbdbf8e4aebb8cfb7fcf2f5c7674
rogeriobnr <rogeriobnr@gmail.com>
2023-04-23 00:05:13 -0300

    build: categories route

--- api-node/routes/routes.js
+++ api-node/routes/routes.js
@@ -3,5 +3,8 @@ module.exports = (app) => {
     app.route("/users")
         .get(app.api.users.list)
         .post(app.api.users.save)
-        
+
+    app.route("/categories")
+        .post(app.api.categories.save)
+
 }
