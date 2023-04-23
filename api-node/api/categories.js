952d0e981a44d78815414bb2691c3f7c2e44e236
rogeriobnr <rogeriobnr@gmail.com>
2023-04-23 00:54:21 -0300

    build: create get method and route categories

--- api-node/api/categories.js
+++ api-node/api/categories.js
@@ -1,20 +1,49 @@
-module.exports = () => {
+module.exports = (app) => {
+    /**
+     * Creation of the Read Method to read the user's insertions into the database.
+     */
+    const read = async (req, res) => {
+        const categories = await app
+            .database('categories')
+            .select("*")
+
+        if (categories.select) {
+            return res.status(200).json({ Mensagem: "Lista exibida" });
+        }
+        return res.json(categories);
+    }
     /**
      * Creation of the Save Method to record the user's insertions into the database.
      */
-    const save = (req, res) => {
+    const save = async (req, res) => {
         const category = { ...req.body };
 
         if (!category.name) {
             return res.status(400).json({ error: "Nome não informado" });
         }
 
+        const categoryExists = await app
+            .database("categories")
+            .where({ name: category.name })
+            .first();
+
+        if (categoryExists) {
+            return res.status(400).json({ error: "Nome já cadastrado" });
+        }
+
         category.image = "category.png";
 
-        return res.json(category)
+        app
+            .database('categories')
+            .insert(category)
+            .then((_) => res.status(200).send())
+            .catch((err) => res.status(500).send())
+
+        if (category.name) {
+            return res.status(200).json({ Mensagem: "Nome cadastrado!" });
+        }
+
 
     }
-
-
-    return { save };
+    return { read, save };
 }
--- api-node/routes/routes.js
+++ api-node/routes/routes.js
@@ -5,6 +5,8 @@ module.exports = (app) => {
         .post(app.api.users.save)
 
     app.route("/categories")
+        .get(app.api.categories.read)
         .post(app.api.categories.save)
 
-}
+
+} 
